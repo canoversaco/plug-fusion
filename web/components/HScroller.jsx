41ffb641f13f7cf6c097cb89933ec9b69563373b
canoversaco <122825510+canoversaco@users.noreply.github.com>
2025-09-09 16:24:32 +0200

    Delete web/components directory

--- web/components/HScroller.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function HScroller({ children }){
-  return (
-    <div className="-mx-4 px-4 overflow-x-auto">
-      <div className="flex gap-3">{children}</div>
-    </div>
-  )
-}
