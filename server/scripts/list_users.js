37e649cdb555cacc15439b491b5913141de87dce
canoversaco <122825510+canoversaco@users.noreply.github.com>
2025-09-12 14:40:02 +0200

    Delete server directory

--- server/scripts/list_users.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config'
-import { query, driver } from '../db/index.js'
-const r = await query('SELECT id, username, role FROM users ORDER BY id ASC', [])
-console.log({ driver, users: r.rows })
-process.exit(0)
